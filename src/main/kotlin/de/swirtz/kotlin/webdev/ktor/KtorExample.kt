f0473af4862a4b611bbfa92ba480a4d3fdef6225
Simon <simon.wirtzq@gmail.com>
2017-10-04 23:10:20 +0200

    init project with ktor simple GET route

---
+++ src/main/kotlin/de/swirtz/kotlin/webdev/ktor/KtorExample.kt
@@ -0,0 +1,17 @@
+import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.host.*
+import org.jetbrains.ktor.http.*
+import org.jetbrains.ktor.netty.*
+import org.jetbrains.ktor.response.*
+import org.jetbrains.ktor.routing.*
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+        }
+    }
+    server.start(wait = true)
+}
